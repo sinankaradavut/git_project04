d01e0c92b9348db2e77813fe14788b452e529cc2
Sinan Karadavut <sinankaradavut84@gmail.com>
2020-08-11 15:29:34 -0400

    this is the forth commit from local repo

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,8 +5,9 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("This is the first changes from local master");
 	    System.out.println("This is the second commit from local master");
-	   System.out.println("This is the third commit from local master");
+	    System.out.println("This is the third commit from local master");
 	  System.out.println("This is the first commit from remote repository");
+	  System.out.println("This is the forth commit from local master");
 	}
 
 }
